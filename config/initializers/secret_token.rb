db8919d69af600a742fb3f9900d8919c7efaac5c
serviceweb2012 <serviceweb@serviceweb.com.br>
2012-05-02 16:42:38 -0300

    primeiro commit estrutura scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EstruturaScaffold::Application.config.secret_token = 'da0030e5a456855534c69ca5f938f630cf819314f4448b27d06390772da226edd71a27794baf10f16a850d8c8b44c24d94d6930eb6ec97e1bdd2a4b00bd0109d'
